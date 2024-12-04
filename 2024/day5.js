5e08393b823f5caa6d1332526e6a236f79442394
Szörényi Miklós <mszorenyi@brighthills.local>
2024-12-04 09:49:08 +0100

    day4

---
+++ 2024/day5.js
@@ -0,0 +1,12 @@
+const test_input = "";
+const final_input = "";
+
+function part1(input) {
+
+}
+
+function part2(input) {
+
+}
+
+part1(test_input);
--- bh-ladder.js
+++ bh-ladder.js
@@ -1,4 +1,5 @@
 const ladder = require('./bh-ladder.json');
+// const moment = require('momentjs');
 const members = Object.values(ladder.members)
 
 const ladderTable = [];
@@ -7,11 +8,14 @@ members.sort((a, b) => b.local_score - a.local_score).forEach((player, index) =>
     place: index + 1,
     name: player.name ?? `(anonymous user #${player.id})`,
     score: player.local_score,
+    first: player.name ?? `(anonymous user #${player.id})`,
   };
 
   Object.values(player.completion_day_level).forEach((day, index) => {
     const diff = day[2] ? day[2].get_star_ts - day[1].get_star_ts : 0;
     data[`day${index + 1}`] = diff ? Math.round(diff / 60) + ':' + (diff % 60) : null;
+    const d = new Date(day[1].get_star_ts * 1000);
+    data[`day${index + 1}`] += ' - ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
   });
 
   ladderTable.push(data);
